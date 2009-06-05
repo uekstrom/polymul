5abad54806b0d7f21c94fa7c5bcd0504e5599a9a
uekstrom <uekstrom@2bc35960-50e0-11de-b541-f52ad3e57354>
2009-06-05 09:55:15 +0000

    Add polynomial::eval(), and update documentation.

--- polymul_test.cpp
+++ polymul_test.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+using namespace std;
 #include "polymul.h"
 
-using namespace std;
+
 
 template<class num, int Nvar, int Ndeg>
 void printpoly(ostream &dst, const polynomial<num,Nvar,Ndeg> &p)
@@ -24,15 +25,32 @@ int main(void)
      80, 134, 185, 367, 365, 153, 400, 534, 326, 
      44, 125, 184, 327, 559, 561, 302, 782, 976, 
      595, 238, 591, 1066, 849, 496};
-  cout << "Starting tests.." << endl;
 
+  int eval_1d_x[] = {11,-13,17};
+  int eval_1d_p[] = {3,5,-7};
+  int eval_1d_px[] = {-789, -1245, -1935};
+
+  cout << "Starting tests.." << endl;
+  
+  // Evaluating
+  polynomial<int,1,2> peval_1d;
+  for (int i=0;i<peval_1d.size();i++)
+    peval_1d[i] = eval_1d_p[i];
+  for (int i=0;i<3;i++)
+    if (peval_1d.eval(&eval_1d_x[i]) != eval_1d_px[i])
+      {
+	cout << "WARNING (polyval 1d), expected " << eval_1d_px[i] <<
+	  " got " << peval_1d.eval(&eval_1d_x[i]) << endl;
+      }
+  
+  // Multiplying
   polynomial<int,3,2> p1, p2, pt;
   polynomial<int,3,4> pp(0),ppset;
   for (int i=0;i<p1.size();i++)
     p1[i] = 7+i;
   for (int i=0;i<p2.size();i++)
     p2[i] = i*i/2 - i;
-
+  
   pp.zero();
   polymul_add(pp,p1,p2);
   polymul(ppset,p1,p2);
@@ -47,6 +65,15 @@ int main(void)
 	  cout << "WARNING (set vs add): " <<  i << " "<< pp[i]<< " " << ppset[i] <<endl;
 	}
     }
+  // Check that multiply then eval gives the same as eval then multiply
+  int x1[3] = {12,-1,7};
+  int x2[3] = {-4,3,1};
+  if (p1.eval(x1)*p2.eval(x1) != pp.eval(x1) || 
+      p1.eval(x2)*p2.eval(x2) != pp.eval(x2))
+    {
+      cout << "WARNING: multidimensional eval failed\n";
+    }
+
   //start of polymul result should match taylormul
   taylormul(pt,p2,p1);
   for (int i=0;i<pt.size();i++)
